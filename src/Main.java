5ed10c5fd57440120e91d7b13584895db2ba9c1c
AhnMinsur <93420148+minwis@users.noreply.github.com>
2024-08-15 23:06:40 +0530

    Completed 02. Fruit Practice

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        //intded ver.
         int apples = 35;
         int oranges = 22;
         int peaches = 18;
@@ -17,6 +18,17 @@ public class Main {
         System.out.println("Bananas = " + bananas);
         System.out.println("Pineapples = " + pineapples);
 
+        //hashmap ver.
+        HashMap<String, Integer> FruitN = new HashMap<>();
+
+        FruitN.put("apples", 35);
+        FruitN.put("oranges", 22);
+        FruitN.put("peaches", 18);
+        FruitN.put("grapes", 10);
+        FruitN.put("bananas", 8);
+        FruitN.put("pineapples", 5);
+
+        System.out.println(FruitN);
 
     }
 }
