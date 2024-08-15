7dc61966e19851838eb1bace52aeb1b9fe9334a6
AhnMinsur <93420148+minwis@users.noreply.github.com>
2024-08-15 23:34:59 +0530

    Half-completed 03. Conversion

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,34 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //intded ver.
-        int apples = 35;
-        int oranges = 22;
-        int peaches = 18;
-        int grapes = 10;
-        int bananas = 8;
-        int pineapples = 5;
+        double inch = 2.54, yard = 0.9144, mile = 1.609, centimeter = 0.3937, meter = 1.094, kilometer = 0.6214; //variables declaration
+        System.out.println(1 + " inch = " + inch + " centimeters\n" +
+                1 + " yard = " + yard + " meters \n" +
+                1 + " mile = " + mile + " kilometers\n" +
+                1 + " centimeter = " + centimeter + " inches\n" +
+                1 + " meter = " + meter + " yards\n" +
+                1 + " kilometer = " + kilometer + " miles"
+                );
 
-        System.out.println("Totals \n ------------ \nApples = " + apples);
-        System.out.println("Oranges = " + oranges);
-        System.out.println("Peaches = " + peaches);
-        System.out.println("Grapes = " + grapes);
-        System.out.println("Bananas = " + bananas);
-        System.out.println("Pineapples = " + pineapples);
+        /*
+        1 inch
+        12 inches = 1 foot
+        3 feet = 1 yard
+        1760 yard = 1 mile
+        1 mile
 
-        //hashmap ver.
-        HashMap<String, Integer> FruitN = new HashMap<>();
+        1cm
+        100cm = 1m
+        1000m = 1km
+         */
+        Scanner sc = new Scanner(System.in);
+        String inputUnit = sc.nextLine();
+        String outputUnit = sc.nextLine();
 
-        FruitN.put("apples", 35);
-        FruitN.put("oranges", 22);
-        FruitN.put("peaches", 18);
-        FruitN.put("grapes", 10);
-        FruitN.put("bananas", 8);
-        FruitN.put("pineapples", 5);
-
-        System.out.println(FruitN);
+        int[] imperial = new int[] {1, 12, 1760, 1};
+        int[] metric = new int[] {1, 100, 1000, 1};
 
     }
+
+
 }
