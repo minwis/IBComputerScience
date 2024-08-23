348e9f4d551c11bf684ebcfb14f2fb660083fceb
AhnMinsur <minwisdom08@gmail.com>
2024-08-23 08:56:39 +0530

    Optimization of 06. Baseball

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         int hits = 27, atBats = 80, roundedAvg = 0;
-        double battingAvg = 0.0;
-
-        battingAvg = (double)hits/atBats;
+        double battingAvg = (double)hits/atBats;;
         battingAvg /= 0.0001;
 
         double n = battingAvg % 10;
         if ( n >= 5) {
-            double output_first_digit = ((battingAvg - n) % 100) / 10 + 1;
-            roundedAvg += output_first_digit;
+            roundedAvg += (int) ((battingAvg - n) % 100) / 10 + 1;
         }
 
-        roundedAvg += ((battingAvg-(battingAvg%100)) / 100) * 10;
+        roundedAvg += (int) (((battingAvg-(battingAvg%100)) / 100) * 10);
 
         System.out.println(roundedAvg);
     }
