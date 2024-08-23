56e691d43ef57d890b8fe8787d7983d052b8591c
AhnMinsur <minwisdom08@gmail.com>
2024-08-23 09:47:08 +0530

    10. Condition Statement Exercise

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Type your name, playa: ");
-        String name = sc.nextLine();
-
-        System.out.print("(M)ale or (F)emale? ");
-        String gender_input = sc.nextLine();
-        char gender = gender_input.charAt(0);
-
-        String firstName = "";
-        String lastName = "";
-        for ( int i = 0; i < name.length(); i++ ) {
-            if ( name.charAt(i) == ' ' ) {
-                firstName = name.substring(0, i);
-                lastName = name.substring(i+1);
-            }
+        int inputN = sc.nextInt();
+        if ( inputN >= 0 ) {
+            System.out.println("The number is positive");
+        }
+        else {
+            System.out.println("The number is negative");
         }
 
-        System.out.print( firstName.charAt(0) + ". " + lastName.toUpperCase());
-
-        if ( gender == 'M' ) {
-            System.out.print(" Daddy ");
+        if ( inputN % 2 == 0 ) {
+            System.out.println("The number is even");
         }
-        else { //if gender == 'F'
-            System.out.print(" Goddess ");
+        else {
+            System.out.println("The number is odd");
         }
 
-        System.out.print(firstName + "-izzle");
+        //Bonus 1
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println( Math.max(n1, n2) );
+
+        //Bonus 2
+        System.out.print("Enter the year number: ");
+        int year = sc.nextInt();
+
+        if ( year % 4 == 0 && year % 100 != 0 ) {
+            System.out.println("\n" + year + "is a leap year");
+        }
+        else {
+            System.out.println("\n" + year + "is NOT a leap year");
+        }
     }
+
 
 }
