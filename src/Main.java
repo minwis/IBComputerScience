0c7989b3a4932ea92d85039e501444e23034b98e
AhnMinsur <minwisdom08@gmail.com>
2024-09-04 08:54:41 +0530

    Complete Escape Sequence practice

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,71 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int inputN = sc.nextInt();
-        if ( inputN >= 0 ) {
-            System.out.println("The number is positive");
-        }
-        else {
-            System.out.println("The number is negative");
-        }
-
-        if ( inputN % 2 == 0 ) {
-            System.out.println("The number is even");
-        }
-        else {
-            System.out.println("The number is odd");
+        //Scanner sc = new Scanner(System.in);
+        /*
+        for ( int i = 1; i  <= 5; i++ ) {
+            for ( int j = 0; j < i; j++ ) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
 
-        //Bonus 1
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        System.out.println( Math.max(n1, n2) );
+        System.out.println();
 
-        //Bonus 2
-        System.out.print("Enter the year number: ");
-        int year = sc.nextInt();
+        for ( int i = 1; i <= 5; i++ ) {
+            for ( int j = 0; j < 10; j++) {
+                System.out.print(" ");
+                if ( 10-(2*i) <= j) {
+                    System.out.print("*");
+                    j++;
+                }
+            }
+            System.out.println();
+        }*/
 
-        if ( year % 4 == 0 && year % 100 != 0 ) {
-            System.out.println("\n" + year + "is a leap year");
+        /*
+        for ( int i = 1; i <= 5; i++ ) {
+            for ( int j = 1; j <= 5; j++ ) {
+                if ( 5 - i < j ) {
+                    System.out.print(" *");
+                }
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
-        else {
-            System.out.println("\n" + year + "is NOT a leap year");
+         */
+        /*System.out.println();
+
+        for ( int i = 1; i <= 20; i++ ) {
+
+            //int J = 1;
+            for ( int j = 1; j <= 20 - i - 1; j++ ) {
+                System.out.print(" ");
+            }
+            for ( int j = 20-i; j <= 20+i; j++ ) {
+                System.out.print(" *");
+            }
+            for ( int j = 20+(2 * i); j <= 40; j++ ) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
         }
+        */
+
+        System.out.print("She said" + " \"Hello\" to me" +
+                "\nI want to write \\" +
+                "\n\tIn this sentence I started with an indention." +
+                "\nMy name is \'Alice\'" +
+                "\n\tHere is some \"Text\" that" +
+                 "\n" +
+                "\nI'd like to be \"escaped\" for Java." +
+                "\n" +
+                "\nI'll try a couple special characters here: \\");
+
+
     }
-
 
 }
