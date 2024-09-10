61840733a7f12c56863f41d4c3bae0b0c16bef48
AhnMinsur <minwisdom08@gmail.com>
2024-09-10 09:23:41 +0530

    Pattern 16 Complete

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
+        //Pattern 1
         for ( int i = 1; i  <= 5; i++ ) {
             for ( int j = 0; j < i; j++ ) {
                 System.out.print("* ");
@@ -14,6 +15,7 @@ public class Main {
 
         System.out.println();
 
+        //Pattern 2
         for ( int i = 1; i <= 5; i++ ) {
             for ( int j = 0; j < 10; j++) {
                 System.out.print(" ");
@@ -25,7 +27,7 @@ public class Main {
             System.out.println();
         }
 
-        /*
+        /* //Another solution for Pattern 2
         for ( int i = 1; i <= 5; i++ ) {
             for ( int j = 1; j <= 5; j++ ) {
                 if ( 5 - i < j ) {
@@ -40,6 +42,7 @@ public class Main {
          */
         System.out.println();
 
+        //Pattern 3
         int starStringLength = 1;
 
         for ( int i = 1; i <= 20; i++ ) {
@@ -48,7 +51,7 @@ public class Main {
                 System.out.print(" ");
             }
 
-            for ( int j = 0; j< starStringLength; j++ ) {
+            for ( int j = 0; j < starStringLength; j++ ) {
                 System.out.print(" *");
             }
 
@@ -57,8 +60,78 @@ public class Main {
 
         }
 
+        System.out.println();
 
+        //Pattern 4
+        for ( int i = 1; i  <= 5; i++ ) {
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
+        System.out.println();
+
+        //Pattern 5
+        System.out.println("How many rows you want in this pattern?");
+        byte NOfRows = sc.nextByte();
+        System.out.println("Here is the pattern! :D");
+        for ( int i = 1; i <= NOfRows; i++ ) {
+            for ( int j = 0; j < i; j++ ) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+        //Pattern 6
+        System.out.println();
+
+        int NOfStar = 1;
+        int increaseStarN = 2;
+        int NOfSpace = 4;
+        int decreaseSpaceN = -1;
+        for ( int i = 1; i <= 9; i++ ) {
+
+            for ( int j = 0; j < NOfSpace; j++ ) {
+                System.out.print(" ");
+            }
+            NOfSpace += decreaseSpaceN;
+
+            for ( int j = 0; j < NOfStar; j++ ) {
+                System.out.print("*");
+            }
+            NOfStar += increaseStarN;
+
+            if ( i == 4 ) {
+                decreaseSpaceN *= -1;
+                increaseStarN *= -1;
+            }
+
+            System.out.println();
+        }
+
+        //Floyd's Triangle
+        System.out.println("\nEnter the number of rows for floyd's triangle:");
+        int FloydN = sc.nextInt();
+        int outputN = 1;
+        for ( int i = 1; i <= FloydN; i++ ) {
+            for ( int j = 0; j < i; j++ ) {
+                System.out.print(outputN++ + " ");
+            }
+            System.out.println();
+        }
+
+        //Final Pattern; Number and Stars
+        System.out.println();
+        for ( int i = 1; i <= 7; i++ ) {
+            for ( int j = 1; j <= i; j++ ) {
+                System.out.print(j);
+            }
+            for ( int j = i + 1; j < 7; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
