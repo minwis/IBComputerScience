427e0fa0c3e9d28314f055cbbf3741acb8c0c4c1
AhnMinsur <minwisdom08@gmail.com>
2024-09-25 12:55:06 +0530

    26 - Creating Email Address
    + Adjusted AES Email Address Generator

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,84 @@ import java.util.*;
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
+    public static String firstName;
+    public static String lastName;
+    public static String gradYear;
 
     public static void main(String[] args) {
-        int i = 0;
-        do {
-            System.out.print(i + " ");
-            i++;
-        } while( i <= 20 );
+        System.out.println("Please enter your first name:");
+        firstName = sc.nextLine();
+        System.out.println("Please enter your last name:");
+        lastName = sc.nextLine();
+        System.out.println("Please enter your graduation year(in [yyyy] format):");
+        gradYear = sc.nextLine();
+
+        System.out.println(firstName.toLowerCase() + lastName.substring(0,1).toLowerCase()+ gradYear.substring(2) + "@stu.koc.k12.tr");
 
         System.out.println();
 
-        i = 1;
-        do {
-            if ( i % 2 == 1 ) {
-                System.out.print(i + " ");
+        //Extra
+        System.out.println("Are you student or teacher?");
+        String role = sc.nextLine();
+        System.out.println("Please enter your first name:");
+        firstName = sc.nextLine();
+
+
+        String secondName = "";
+        if (role.equals("student") || role.equals("Student")) {
+            System.out.println("Please enter your middle/second name:");
+            secondName = sc.nextLine();
+        }
+
+        System.out.println("Please enter your last name:");
+        lastName = sc.nextLine();
+
+        gradYear = "";
+        if (role.equals("student") || role.equals("Student")) {
+            System.out.println("Please enter your graduation year:");
+            gradYear = sc.nextLine();
+        }
+
+        String firstCharOfSecName = "";
+
+        if ( !secondName.isEmpty()) {
+            firstCharOfSecName = secondName.substring(0,1).toUpperCase() + secondName.substring(1);
+        }
+        System.out.println(firstName.toLowerCase() + firstCharOfSecName + lastName.charAt(0) + gradYear + "@koc.k12.tr");
+
+        System.out.println();
+
+        //AES Email Generator
+        // --> numbers added at the end of the address when the students' grad year, first name and last name overlap
+        //is not implemented.
+        //The number requires efficient and comprehensive index data structure
+
+        System.out.println("Welcome to AES!\nAre you a student or a teacher");
+        role = sc.nextLine();
+        System.out.println("What is your name (first, second, last name)?");
+        String name = sc.nextLine();
+
+        String firstCharName = name.substring(0,1).toLowerCase();
+
+        int i = 0;
+        int spaceIndex = 0;
+        while ( i < name.length() ) {
+            if ( name.charAt(i) == ' ' ) {
+                spaceIndex = i;
             }
             i++;
-        } while ( i <= 50 );
+        }
+        lastName = name.substring(spaceIndex + 1);
 
-        System.out.println();
+        System.out.println("What is your graduation year? (Format: [yyyy])");
+        if ( role.charAt(0) == 's' || role.charAt(0) == 'S') {
+            gradYear = sc.nextLine();
+        }
 
-        i = 0;
-        do {
-            System.out.print(i + " ");
-            i+=10;
-        } while( i <= 200 );
+        String email = gradYear.substring(2) + firstCharName + lastName.toLowerCase();
 
-        System.out.println();
+        System.out.println( email + "@aes.ac.in");
 
-        i = 97;
-        do {
-            System.out.print((char)i + " ");
-            i++;
-        } while ( i <= 122 );
     }
 
 }
