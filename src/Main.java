7b2cd4b65953790cc3cc4b7742da320deee8c97e
AhnMinsur <minwisdom08@gmail.com>
2024-09-27 09:40:54 +0530

    28 - Palindrome Assignment #5

--- src/Main.java
+++ src/Main.java
@@ -5,45 +5,30 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        String[] substitutingWords = new String[13];
-        for ( int i = 1; i <= 12; i++ ) {
-            System.out.print("[" + i + "] ");
-            if ( i == 2 ) {
-                System.out.print("Enter a type of VEHICLE --> ");
+        System.out.println("Enter a word and I will determine if it is a palindrome.");
+
+        String Continue = "Y";
+        while (Continue.equals("Y")) {
+            System.out.print("\nWord --> ");
+            String word = sc.nextLine();
+
+            boolean isPalindrome = true;
+            for ( int i = 0; i < word.length(); i++ ) {
+                if ( word.charAt(i) != word.charAt(word.length() - 1 - i) ) {
+                    isPalindrome = false;
+                }
             }
-            else if ( i == 8 ) {
-                System.out.print("Enter a VERB ending in 'ed --> ");
-            }
-            else if ( i == 3 || i == 4 || i == 7 ) {
-                System.out.print("Enter an ADJECTIVE --> ");
+
+            if ( isPalindrome ) {
+                System.out.println(word + " is a palindrome");
             }
             else {
-                System.out.print("Enter a NOUN --> ");
+                System.out.println(word + " is not a palindrome");
             }
-            substitutingWords[i] = sc.nextLine();
+
+            System.out.print("\ncontinue[Y/N]? ");
+            Continue = sc.nextLine();
         }
-
-        System.out.println("Just sit right back and you`ll hear a tale,");
-        System.out.println("A tale of a fateful " + substitutingWords[1] + ",");
-        System.out.println("That started from this tropic port,");
-        System.out.println("Aboard this tiny " + substitutingWords[2] + ".");
-        System.out.println();
-        System.out.println("The mate was a " + substitutingWords[3]+ " sailor man,");
-        System.out.println("The skipper " + substitutingWords[4]+ " and sure.");
-        System.out.println("Five + " + substitutingWords[5] + " set sail that day,");
-        System.out.println("For a three hour " + substitutingWords[6] + ", a three hour " + substitutingWords[6] + ".");
-        System.out.println();
-        System.out.println("The weather started getting " + substitutingWords[7] + ",");
-        System.out.println("The tiny " + substitutingWords[2]+ " was " + substitutingWords[8] + ",");
-        System.out.println("If not for the " + substitutingWords[9] + " of the fearless crew ,");
-        System.out.println("The " + substitutingWords[10] + " would be lost, the " + substitutingWords[10] +" would be lost.");
-        System.out.println();
-        System.out.println("The " + substitutingWords[2] + " set ground on the shore of this uncharted desert isle,");
-        System.out.println("With Gilligan, The Skipper too,");
-        System.out.println("The millionaire and his " + substitutingWords[11] + " , The movie " + substitutingWords[12] + ",");
-        System.out.println("And The Rest, Here on Gilligan`s Isle.");
-
-
     }
 
 }
