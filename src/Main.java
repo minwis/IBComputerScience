51df8cb1535812056a393d9fedb0b26be1248d0c
AhnMinsur <minwisdom08@gmail.com>
2024-10-16 12:48:54 +0530

    30_Array Practice_02

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,68 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int[] A = new int[5];
-        A[0] = 2;
-        A[1] = 4;
-        A[2] = 6;
-        A[3] = 8;
-        A[4] = 10;
-        for ( int i = 0; i < 5; i++ ) {
-            System.out.print( A[i] + " " );
+
+        //Write a Java Program to find the second highest value in a numeric array.
+        int[] array1 = new int[10];
+
+        int highestValue = -1;
+        int secondValue = -1;
+
+        for ( int j = 0; j < 10; j++ ) {
+            array1[j] = (int)(Math.random() * 100);
         }
 
-        System.out.println();
-        int[] B = new int[] {10, 20, 30, 40, 50};
-        for ( int i = 0; i < 5; i++ ) {
-            System.out.print( B[i] + " " );
+        for (int k : array1) {
+            if (highestValue < k) {
+                highestValue = k;
+            }
         }
 
-        System.out.println();
-        String[] C = new String[3];
-        C[0] = "Dog";
-        C[2] = "Cat";
-        for ( int i = 0; i < 3; i++ ) {
-            System.out.print( C[i] + " " );
+        for (int j : array1) {
+            if (secondValue < j && j < highestValue) {
+                secondValue = j;
+            }
         }
 
-        System.out.println();
-        double[] D = new double[4];
-        D[0] = 5.6;
-        D[1] = 9.9;
-        for ( int i = 0; i < 3; i++ ) {
-            System.out.print( C[i] + " " );
+        System.out.println(secondValue);
+
+        //Write a Java Program that merge two string arrays.
+        String[] array2 = new String[] { "sea otters", "are", "cuter", "than", "cats", ":DD" };
+        String[] array3 = new String[] { "my", "girlfriend", "is", "on", "her", "period", "HELP!!!" };
+        int totalLength = array2.length + array3.length;
+        String[] array4 = new String[totalLength];
+
+        for ( int i = 0; i < totalLength; i++ ) {
+            if ( i < array2.length ) {
+                array4[i] = array2[i];
+            }
+            else {
+                array4[i] = array3[i-array2.length];
+            }
         }
 
-        System.out.println();
-        String[] E = new String[10];
-        for ( int i = 0; i < 10; i ++ ) {
-            E[i] = sc.nextLine();
+        System.out.println(Arrays.toString(array4));
+
+        //Write a Java Program to reverse the array contents inside the array.
+        String[] arr5 = new String[] { "please", "not", "the", "formative" };
+        String[] reversedArr5 = new String[arr5.length];
+        for ( int i = 1; i <= arr5.length; i++ ) {
+            reversedArr5[arr5.length - i] = arr5[i - 1];
         }
-        for ( int i = 0; i < 3; i++ ) {
-            System.out.print( E[i] + " " );
+        System.out.println(Arrays.toString(reversedArr5));
+
+        //Write a Java program to move all 0's to the end of an array. Maintain the relative order of the other (non-zero) array elements.
+        int[] arr6 = new int[] { 0, 4, 16, 0, 5, 7, 0, 9, 3, 99, 0 };
+        int[] orderedArr6 = new int[arr6.length];
+        int index = 0;
+        for ( int i = 0; i < arr6.length; i++ ) {
+            if ( arr6[i] != 0 ) {
+                orderedArr6[index] = arr6[i];
+                index++;
+            }
         }
 
-        System.out.println();
-        int[] F = new int[20];
-        for ( int i = 0; i < 20; i ++ ) {
-            System.out.print("[" + (i+1) + "]");
-            F[i] = sc.nextInt();
-        }
-        for ( int i = 19; i >= 0; i-- ) {
-            System.out.print( F[i] + " " );
-        }
+        System.out.println(Arrays.toString(orderedArr6));
 
     }
 
